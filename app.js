bed77d3b3dfa66694495578f7a6e3435e9bf263d
Kacper <kacper.adamski95@gmail.com>
2023-02-20 08:02:00 +0100

    finished two first sections

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp ({
+    data() {
+        return {
+            counterInstall: 0,
+            counterLogin: 0,
+            counterSetup: 0
+        }
+    }
+});
+
+app.mount('#assignment')
