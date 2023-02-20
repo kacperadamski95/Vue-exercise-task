0e88dea28b5b7fbab8dbb4543c4a074967f11c32
Kacper Adamski <kacper.adamski95@gmail.com>
2023-02-20 22:24:58 +0100

    finished with landing page

--- app.js
+++ app.js
@@ -1,11 +1,104 @@
-const app = Vue.createApp ({
-    data() {
-        return {
-            counterInstall: 0,
-            counterLogin: 0,
-            counterSetup: 0
-        }
-    }
-});
+const number1 = Vue.createApp({
+  data() {
+    return {
+      number: 25,
+      tweened: 0,
+    };
+  },
+  computed: {
+    animatedNumber() {
+      return Math.floor(this.tweened);
+    },
+  },
+  mounted() {
+    gsap.to(this.$data, {
+      duration: 4,
+      tweened: this.number,
+    });
+  },
+}).mount("#number1");
 
-app.mount('#assignment')
+const number2 = Vue.createApp({
+  data() {
+    return {
+      number: 1200,
+      tweened: 0,
+    };
+  },
+  computed: {
+    animatedNumber() {
+      return Math.floor(this.tweened);
+    },
+  },
+  mounted() {
+    gsap.to(this.$data, {
+      duration: 1,
+      tweened: this.number,
+    });
+  },
+}).mount("#number2");
+
+const number3 = Vue.createApp({
+  data() {
+    return {
+      number: 14,
+      tweened: 0,
+    };
+  },
+  computed: {
+    animatedNumber() {
+      return Math.floor(this.tweened);
+    },
+  },
+  mounted() {
+    gsap.to(this.$data, {
+      duration: 3,
+      tweened: this.number,
+    });
+  },
+}).mount("#number3");
+
+const number4 = Vue.createApp({
+  data() {
+    return {
+      number: 3500,
+      tweened: 0,
+    };
+  },
+  computed: {
+    animatedNumber() {
+      return Math.floor(this.tweened);
+    },
+  },
+  mounted() {
+    gsap.to(this.$data, {
+      duration: 1.5,
+      tweened: this.number,
+    });
+  },
+}).mount("#number4");
+
+const counters = Vue.createApp({
+  data() {
+    return {
+      clickCounters: {
+        link1: 0,
+        link2: 0,
+        link3: 0,
+      },
+      activeElement: null,
+    };
+  },
+  methods: {
+    handleClick(linkNumber) {
+      const linkRef = `link${linkNumber}`;
+      if (this.activeElement !== linkRef) {
+        this.activeElement = linkRef;
+        this.clickCounters[linkRef]++;
+        console.log(
+          `Kliknąłeś/aś w ${linkNumber} link. Suma kliknięć: ${this.clickCounters[linkRef]}`
+        );
+      }
+    },
+  },
+}).mount("#counter");
